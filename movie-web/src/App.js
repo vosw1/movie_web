4d98601c465ae8712033e93669260a86f51c3eee
vosw1 <vosw1@naver.com>
2024-09-22 23:52:55 +0900

    CRA 사용하기

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welcome React.JS</h1>
+      <h1 className={styles.title}>Welcome React.JS</h1>
+      <Button text={"계속"} />
     </div>
   );
 }
