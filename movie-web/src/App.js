03148b0f20dfcab1b79de4c325324bae76047610
vosw1 <vosw1@naver.com>
2024-09-23 10:56:30 +0900

    useEffect 사용하기2

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -2,15 +2,33 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
     console.log("i run only once");
   }, []);
 
+  useEffect(() => {
+    if(keyword !== ""){
+      console.log("i run when 'keyword' changes" , keyword);
+    }
+  }, [keyword]);
+  
+  useEffect(() => {
+      console.log("i run when 'counter' changes" , counter);
+  }, [counter]);
+
   return (
     <div>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search Here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
