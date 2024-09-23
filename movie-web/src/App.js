ccf1f5bb5557d1ad472582a0cabbfb6c3158a75d
vosw1 <vosw1@naver.com>
2024-09-23 10:26:34 +0900

    useEffect 사용하기

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -1,11 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome React.JS</h1>
-      <Button text={"계속"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
