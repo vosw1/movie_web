a4722c97abfbc6d6ff06e5ce1f30b19343e09015
vosw1 <vosw1@naver.com>
2024-09-23 11:44:32 +0900

    cleanup 사용하기

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -1,36 +1,24 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
+function Hello() {
   useEffect(() => {
-    console.log("i run only once");
+    console.log("hi :)");
+    return () => {
+      console.log("bye :(");
+    };
   }, []);
-
-  useEffect(() => {
-    if(keyword !== ""){
-      console.log("i run when 'keyword' changes" , keyword);
-    }
-  }, [keyword]);
   
-  useEffect(() => {
-      console.log("i run when 'counter' changes" , counter);
-  }, [counter]);
+  return <h1>Hello</h1>;
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  
   return (
     <div>
-      <input 
-        value={keyword}
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search Here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
